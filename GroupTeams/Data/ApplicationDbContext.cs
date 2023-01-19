b68dd850875e9f97895dc515b3eab05a2fd0c287
David Oster aka George Pasparakis <paspa@hotmail.com>
2023-01-19 12:17:16 +0200

    Add project files.

---
+++ GroupTeams/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GroupTeams.Models;
+
+namespace GroupTeams.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GroupTeams.Models.Team> Team { get; set; } = default!;
+    }
+}
